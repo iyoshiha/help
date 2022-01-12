415544296c0bd6767c87da5ed0c1d821356b0b6f
Ito Yoshiharu <iyoshiha@c1r21s3.42tokyo.jp>
2022-01-12 19:03:33 +0900

    done bi ring

---
+++ include/push_swap.h
@@ -0,0 +1,16 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "../lst/libft_bonus.h"
+
+
+#define NIL	0x7FFFFFFFFFFFFFFF
+
+
+void	swap(t_list *nil);
+void	push(t_list *dst_nil, t_list *src_nil);
+void	rotate(t_list *nil);
+void	reverse_rotate(t_list *nil);
+
+
+#endif
---
+++ lst/push_swap_cmd.c
@@ -0,0 +1,74 @@
+#include "../include/push_swap.h"
+
+void	swap(t_list *nil)
+{
+	t_list	*second;
+	t_list	*top;
+
+
+	// first->prev = NULL, first->next = second ; => first->prev = second, first->next = third
+	top = nil->next;
+	second = top->next; // second->prev = first , second->next = third ; => second->prev = NULL, second->next = first;
+
+	top->next = second->next;
+	top->prev = second;
+	second->next = top;
+	second->prev = nil;
+}
+
+void	push(t_list *dst_nil, t_list *src_nil)
+{
+	// mv first element of src to top of dst
+	// del top of dst; (second prev points nil)
+	t_list	*dst_top;
+	t_list	*src_top;
+
+	dst_top = dst_nil->next;
+	src_top = src_nil->next;
+
+	src_nil->next = src_top->next;
+	(src_top->next)->prev = src_nil;
+
+	// src : nil, src->next : top;
+	src_top->prev = dst_nil;
+	src_top->next = dst_top;
+	// done mv to dst
+	dst_nil->next = src_top;
+	dst_top->prev = src_top;
+	// done dst stack
+}
+
+void	rotate(t_list *nil)
+{
+	t_list *top;
+	t_list *btm;
+	t_list *second;
+
+	top = nil->next;
+	btm = nil->prev;
+	second = top->next;
+
+	top->next = nil;
+	top->prev = btm;
+	nil->next = second;
+	nil->prev = top;
+	btm->next = top;
+	second->prev = nil;
+}
+
+void	reverse_rotate(t_list *nil)
+{
+	t_list *top;
+	t_list *btm;
+	t_list *btm_2nd;
+
+	top = nil->next;
+	btm = nil->prev;
+	btm_2nd = btm->prev;
+	btm->next = top;
+	btm->prev = nil;
+	nil->next = btm;
+	nil->prev = btm_2nd;
+	btm_2nd->next = nil;
+	top->prev = btm;
+}
