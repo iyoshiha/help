9de3d329390964f89d41b51dbef024435bdb53e2
Ito Yoshiharu <iyoshiha@c1r21s3.42tokyo.jp>
2022-01-15 16:08:24 +0900

    init ps.c

--- push_swap.c
+++ push_swap.c
@@ -1,10 +1,34 @@
 #include "include/push_swap.h"
 
+void push_swap(int len, char **argv)
+{
+	int i;
+	int *num;
+
+	num = malloc(sizeof(int) * len);
+	if (!num)
+		exit(1);
+	i = 0;
+	while (argv[i + 1])
+	{
+		num[i] = ft_mod_atoi(argv[i + 1]);
+
+	}
+
+	return ;
+}
+
 int main(int argc, char **argv)
 {
 	int i;
+	int len;
 
 	ft_mod_atoi(argv[i]);
+	len = argc - 1;
+	push_swap(len);
+
+
+
 
 
 
